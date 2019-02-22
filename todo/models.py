35d31b60b469bea389a0c831a39d87b2e55b1f4c
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2019-02-22 03:54:33 -0300

    Task now has a 'creation_time' field. Fixed 'state' field.

--- todo/models.py
+++ todo/models.py
@@ -1,20 +1,24 @@
 from django.db import models
-from rest_framework import serializers
-import enum
-
-
-class TaskState(enum.Enum):
-    TODO = "TODO"
-    STARTED = "STARTED"
-    WAITING = "WAITING"
-    DONE = "DONE"
-    CANCELLED = "CANCELLED"
 
 
 class Task(models.Model):
+    TODO = 'T'
+    STARTED = 'S'
+    WAITING = 'W'
+    DONE = 'D'
+    CANCELLED = 'C'
+    TASK_STATE_CHOICES = (
+        (TODO, 'TODO'),
+        (STARTED, 'STARTED'),
+        (WAITING, 'WAITING'),
+        (DONE, 'DONE'),
+        (CANCELLED, 'CANCELLED')
+    )
+
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200, default="")
-    state = models.CharField(max_length=1, choices=[(t, t.value) for t in TaskState])
+    state = models.CharField(max_length=1, choices=TASK_STATE_CHOICES, default=TODO)
+    creation_time = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
 
     def __str__(self):
--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     """"
     API endpoint for listing tasks.
     """
-    queryset = Task.objects.filter(archived=False)
+    queryset = Task.objects.filter(archived=False).order_by('-creation_time')
     serializer_class = TaskSerializer
 
 
@@ -15,5 +15,5 @@ class ArchivedViewSet(viewsets.ModelViewSet):
     """
     API endpoint for archived tasks.
     """
-    queryset = Task.objects.filter(archived=True)
+    queryset = Task.objects.filter(archived=True).order_by('-creation_time')
     serializer_class = TaskSerializer
