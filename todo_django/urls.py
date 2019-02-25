01db247147772e27ecb6a0fdaab8274cb5182f0d
Fernando C. Bertoldi <fernando.bertoldi@loadsmart.com>
2019-02-25 16:35:32 -0300

    Adding automatically generated documentation.

--- todo_django/urls.py
+++ todo_django/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('api/', include('todo.urls')),
     path('admin/', admin.site.urls),
     path('schema/', get_schema_view(title='Todo API')),
+    path('docs/', include_docs_urls(title='Todo API')),
 ]
